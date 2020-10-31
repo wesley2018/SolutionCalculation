72bfa50ecc3316338be00e65ed43e46e65937779
wesley <leitewesleyleite@gmail.com>
2020-10-31 10:12:06 -0300

    Criado as soluções

---
+++ Calculos.app/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Calculos.app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b;
+
+            Console.WriteLine("Soma");
+
+            Console.Write("Insira o valor de A: ");
+            Double.TryParse(Console.ReadLine(), out a);
+
+            Console.Write("Insira o valor de B: ");
+            Double.TryParse(Console.ReadLine(), out b);
+
+            Soma(a, b);
+        }
+
+        static void Soma(double a1, double b1)
+        {
+            double soma = (a1 + b1);
+
+            Console.WriteLine("\nSoma é igual a: " + soma);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MaiorValor.app/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MaiorValor.app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+
+            Console.WriteLine("Maior Valor entre duas variáveis inteiras");
+
+            Console.Write("Insira o valor de A: ");
+            Int32.TryParse(Console.ReadLine(), out a);
+
+            Console.Write("Insira o valor de B: ");
+            Int32.TryParse(Console.ReadLine(), out b);
+
+            Comparacao(a, b);
+        }
+
+        static void Comparacao(int a1, int b1)
+        {
+            if (a1 > b1)
+            {
+                Console.WriteLine("A variável A com o valor " + a1 + " é maior que a variável B que possui valor " + b1 );
+            }
+            else
+            {
+                Console.WriteLine("A variável B com o valor " + b1 + " é maior que a variável A que possui valor " + a1);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
